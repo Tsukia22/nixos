5a195f40176d29ab44ad8bd937f4700b94432a32
Tsukia <tsukia@acme.com>
2025-08-27 16:12:52 +0200

    Added Desktop KDE Plasma

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ./modules/locale.nix
       ./modules/users/Tsukia
+      ./modules/Desktops/KDEPlasma
     ];
 
   # Bootloader.
@@ -30,10 +31,6 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
