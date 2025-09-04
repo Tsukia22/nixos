4d23eb2b958485326fbeeedd98e7688141f63890
Tsukia <tsukia@acme.com>
2025-09-04 17:56:36 +0200

    Added port

--- hosts/laptop/tsuacer/configuration.nix
+++ hosts/laptop/tsuacer/configuration.nix
@@ -18,17 +18,21 @@
   };
   # Networking
   networking.hostName = "tsuacer";
-  #networking.firewall.allowedTCPPorts = [  ];
+  networking.firewall.allowedTCPPorts = [ 8384 ];
   services.openssh = {
     enable = true;
     ports = [ 1993 ];
   };
 
   services.syncthing = {
-    enable = true;
-    openDefaultPorts = true; # Open ports in the firewall for Syncthing
-    
+      enable = true;
+      openDefaultPorts = true; # opens TCP/UDP 22000, UDP 21027
+      settings.gui = {
+        user = "myuser";
+        password = "mypassword";
+        address = "0.0.0.0:8384";
   };
+
   system.stateVersion = "25.05"; 
 
 }
