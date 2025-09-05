6469a92b9cd274e36defff5babd2e736b3a0e9be
Xanedithas <xanedithas@gmail.com>
2025-09-05 16:46:02 +0200

    attempt podman

--- hosts/server/xan01/configuration.nix
+++ hosts/server/xan01/configuration.nix
@@ -5,6 +5,7 @@
       /etc/nixos/hardware-configuration.nix
       ./../../../modules/default.nix
       ./../../../modules/users/xanedithas.nix
+      ./../../../modules/podman.nix
     ];
 
   # Bootloader
