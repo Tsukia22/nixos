b573b81e1fdf4352d3d69edb31cbe9715967b3c7
Tsukia22 <67642144+Tsukia22@users.noreply.github.com>
2025-09-08 13:41:35 +0200

    re-enable fail2ban

--- modules/default.nix
+++ modules/default.nix
@@ -48,17 +48,17 @@ in
   };
 
   # Fail2ban
-  # services.fail2ban = {
-  #   enable = true;
-  #   maxretry = 3;
-  #   bantime = "12h";
-  #   bantime-increment = {
-  #     enable = true;
-  #     multipliers = "1 2 4 8 16 32 64";
-  #     maxtime = "168h";
-  #     overalljails = true;
-  #   };
-  # };
+  services.fail2ban = {
+    enable = true;
+    maxretry = 3;
+    bantime = "12h";
+    bantime-increment = {
+      enable = true;
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h";
+      overalljails = true;
+    };
+  };
 
   nix.extraOptions = ''
   experimental-features = nix-command flakes
