a1b158da494663cd1b17aec57f127c7715e19b72
Tsukia <tsukia@acme.com>
2025-09-08 10:49:56 +0200

    compose

--- modules/podmansecond.nix
+++ modules/podmansecond.nix
@@ -1,7 +1,11 @@
-{ ... }: {
+{ config, pkgs, ... }: {
 
 virtualisation.podman.enable = true;
 virtualisation.podman.dockerCompat = true;
 virtualisation.podman.dockerSocket.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    podman-compose
+  ];
 
 }
