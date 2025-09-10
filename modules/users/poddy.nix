6776aa2396b25ee7b501446ff409bad718b4d430
Xanedithas <xanedithas@gmail.com>
2025-09-10 12:01:29 +0200

    enable systemd linger for poddy

--- modules/users/poddy.nix
+++ modules/users/poddy.nix
@@ -3,6 +3,7 @@
     isNormalUser = true;
     description = "poddy";
     extraGroups = [ "docker" "podman" ];
+    linger = true;
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGY2K6YGEJZ5zh24e2rr+lOk/IXEo7DQ08bHnohGvI/s xanedithas"
