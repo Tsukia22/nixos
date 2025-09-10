ad4305d3a319c3cef366b3e3991a2c2c43f198f5
Xanedithas <xanedithas@gmail.com>
2025-09-10 15:47:54 +0200

    add user packages

--- modules/users/xanedithas.nix
+++ modules/users/xanedithas.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   users.users.xanedithas = {
     isNormalUser = true;
     description = "Xanedithas";
@@ -7,6 +7,10 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGY2K6YGEJZ5zh24e2rr+lOk/IXEo7DQ08bHnohGvI/s xanedithas"
+    ];
+
+    packages = with pkgs; [
+      tree
     ];
   };
 }
