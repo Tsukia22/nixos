d039525707d986994949f0be1004327735a4b8ed
Xanedithas <xanedithas@gmail.com>
2025-09-10 11:41:49 +0200

    remove poddy groups

--- modules/users/poddy.nix
+++ modules/users/poddy.nix
@@ -2,7 +2,7 @@
   users.users.poddy = {
     isNormalUser = true;
     description = "poddy";
-    extraGroups = [ "networkmanager" "wheel" "docker" "podman" ];
+    extraGroups = [ "docker" "podman" ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGY2K6YGEJZ5zh24e2rr+lOk/IXEo7DQ08bHnohGvI/s xanedithas"
