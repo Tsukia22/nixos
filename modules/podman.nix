e8e65c26565ceaca9d4de9c43b1f8e8ec54b2a27
Xanedithas <xanedithas@gmail.com>
2025-09-10 15:28:54 +0200

    remove invalid options

--- modules/podman.nix
+++ modules/podman.nix
@@ -5,8 +5,6 @@
     description = "poddy";
     extraGroups = [ "wheel" "docker" "podman" ];
     linger = true;
-    home.group = "poddy";
-    home.permissions = "770";
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGY2K6YGEJZ5zh24e2rr+lOk/IXEo7DQ08bHnohGvI/s xanedithas"
