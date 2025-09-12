36b739a71869bb48f17c5939d9b1e02718118972
Tsukia <tsukia@acme.com>
2025-09-12 14:39:58 +0200

    Remove groups wheel, docker, podman

--- modules/users/xanedithas.nix
+++ modules/users/xanedithas.nix
@@ -2,7 +2,7 @@
   users.users.xanedithas = {
     isNormalUser = true;
     description = "Xanedithas";
-    extraGroups = [ "networkmanager" "wheel" "docker" "podman" ];
+    extraGroups = [ "networkmanager" "wheel" ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGY2K6YGEJZ5zh24e2rr+lOk/IXEo7DQ08bHnohGvI/s xanedithas"
