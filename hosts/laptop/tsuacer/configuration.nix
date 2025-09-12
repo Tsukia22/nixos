b1f3b59ebc0b08b50a72d16ed996577ceb028dd9
Tsukia22 <67642144+Tsukia22@users.noreply.github.com>
2025-09-12 14:59:42 +0200

    Open port range

--- hosts/laptop/tsuacer/configuration.nix
+++ hosts/laptop/tsuacer/configuration.nix
@@ -12,7 +12,8 @@
 
   # Networking
   networking.hostName = "tsuacer";
-  networking.firewall.allowedTCPPorts = [ 5000-5010 ];
+  networking.firewall.allowedTCPPorts = [ 5001 ];
+  networking.firewall.allowedTCPPortRanges = [{ from = 25550; to = 25600; }];
   services.openssh = {
     enable = true;
     ports = [ 1993 ];
