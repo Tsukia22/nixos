df9f4eb9545f9bb23c811afd6884acf0d8db7372
Tsukia22 <67642144+Tsukia22@users.noreply.github.com>
2025-09-24 15:41:39 +0200

    Updated Configuration

--- hosts/server/tsu01/configuration.nix
+++ hosts/server/tsu01/configuration.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:{
 
   imports =
-    [ # Include the results of the hardware scan.
-       /etc/nixos/hardware-configuration.nix # Change this eventually
-      
-      # tsu01
+    [
+      ./hardware-configuration.nix
       ./../../../modules/default.nix
       ./../../../modules/users/tsukia.nix
+      ./../../../modules/podman.nix
     ];
 
   # Networking
   networking.hostName = "tsu01";
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
   networking.firewall.allowedTCPPorts = [ 443 80 ];
   networking.firewall.allowedTCPPortRanges = [
     { from = 25560; to = 25564; } # local use
