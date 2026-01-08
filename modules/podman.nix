c2ed0279113de947531391e243035af80a7cbade
Xanedithas <xanedithas@gmail.com>
2026-01-08 21:59:18 +0100

    add systemd container boot on restart policy

--- modules/podman.nix
+++ modules/podman.nix
@@ -16,4 +16,35 @@
   environment.systemPackages = with pkgs; [
     podman-compose
   ];
+
+  systemd.user.services.podman-restart = {
+    description = "Podman start containers with restart policy unless-stopped on boot";
+    documentation = [ "man:podman-start(1)" ];
+
+    wants = [ "network-online.target" ];
+    after  = [ "network-online.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      Environment = "LOGGING=--log-level=info";
+
+      ExecStart =
+        "${pkgs.podman}/bin/podman $LOGGING start --all --filter restart-policy=unless-stopped";
+
+      ExecStop =
+        "${pkgs.podman}/bin/podman $LOGGING stop --all --filter restart-policy=unless-stopped";
+    };
+
+    install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  /*
+    # To test without homemanager automation
+    systemctl --user daemon-reexec
+    systemctl --user enable podman-restart.service
+    systemctl --user start podman-restart.service
+  */
 }
