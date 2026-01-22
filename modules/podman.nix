b0caf43cabd8a0acfb995c870986d479a8fe94fe
Xanedithas <xanedithas@gmail.com>
2026-01-22 15:38:53 +0100

    try restart script

--- modules/podman.nix
+++ modules/podman.nix
@@ -17,17 +17,31 @@
     podman-compose
   ];
 
-  # Automatically start containers should-start-on-boot
-  systemd.services.podman-autostart = {
+  # # Automatically start containers should-start-on-boot
+  # systemd.services.podman-autostart = {
+  #   enable = true;
+  #   after = [ "podman.service" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   description = "Automatically start containers should-start-on-boot";
+  #   serviceConfig = {
+  #     Type = "idle";
+  #     User = "kami";
+  #     ExecStartPre = ''${pkgs.coreutils}/bin/sleep 1'';
+  #     ExecStart = ''/run/current-system/sw/bin/podman restart --all --filter should-start-on-boot=true'';
+  #   };
+  # };
+
+  systemd.services.podman-restart = {
     enable = true;
     after = [ "podman.service" ];
     wantedBy = [ "multi-user.target" ];
-    description = "Automatically start containers should-start-on-boot";
+    description = "Automatically restart containers";
     serviceConfig = {
       Type = "idle";
       User = "kami";
-      ExecStartPre = ''${pkgs.coreutils}/bin/sleep 1'';
-      ExecStart = ''/run/current-system/sw/bin/podman restart --all --filter restart-policy=always'';
+      ExecStartPre = "/bin/echo 'Restarting containers...'";
+      ExecStart = ''xargs -r -n 1 podman restart < /home/kami/running'';
+      ExecStartPost = "/bin/echo 'Done restarting containers.'";
     };
   };
 }
