f04c35bf9f2b72bcc247888c4689058aab11c826
Xanedithas <xanedithas@gmail.com>
2026-05-02 16:28:56 +0200

    restructure wireguard

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -1,39 +1,47 @@
 { config, pkgs, ... }: {
 
   networking.wg-quick.interfaces = {
-  # Host xan01
-    wg-xan01 = {
-      address = [ "10.100.0.1/24" ];
-      listenPort = 51820;
-      privateKeyFile = "/root/wireguard/wg-xan01-private.key";
+    # Mesh for peer-peer backhaul
+    wg-mesh = {
+      address = [ "10.100.0.2/24" ];
+      listenPort = 51821;
+      privateKeyFile = "/root/wireguard/wg-mesh-private.key";
       postUp = [
-        "wg addconf wg-xan01 /root/wireguard/wg-xan01-peers.conf"
-        "iptables -A FORWARD -i wg-xan01 -j ACCEPT"
+        "wg addconf wg-mesh /root/wireguard/wg-mesh-peers.conf"
+        "iptables -A FORWARD -i wg-mesh -j ACCEPT"
         "iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
       ];
       preDown = [
-        "iptables -D FORWARD -i wg-xan01 -j ACCEPT"
+        "iptables -D FORWARD -i wg-mesh -j ACCEPT"
         "iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
       ];
     };
-
-    # Client xan02
-    wg-xan02 = {
-      address = [ "10.200.0.2/32" ];
-      privateKeyFile = "/root/wireguard/wg-xan02-private.key";
+    # Network for clients connected to this server hub-spoke
+    wg-net = {
+      address = [ "10.200.0.1/32" ];
+      listenPort = 51822;
+      privateKeyFile = "/root/wireguard/wg-net-private.key";
       postUp = [
-        "wg addconf wg-xan02 /root/wireguard/wg-xan02-peers.conf"
+        "wg addconf wg-net /root/wireguard/wg-net-peers.conf"
+        "iptables -A FORWARD -i wg-net -j ACCEPT"
+        "iptables -t nat -A POSTROUTING -s 10.200.0.0/24 -j MASQUERADE"
+      ];
+      preDown = [
+        "iptables -D FORWARD -i wg-net -j ACCEPT"
+        "iptables -t nat -D POSTROUTING -s 10.200.0.0/24 -j MASQUERADE"
       ];
     }
   };
 
   networking.firewall = {
     enable = true;
-    allowedUDPPorts = [ 51820 ];
-    trustedInterfaces = [ "wg-xan01 wg-xan02" ];
+    allowedUDPPorts = [ 51821 51822 ];
+    trustedInterfaces = [ "wg-mesh wg-net" ];
     extraCommands = ''
-      iptables -A FORWARD -i wg-xan01 -j ACCEPT
-      iptables -A FORWARD -o wg-xan01 -j ACCEPT
+      iptables -A FORWARD -i wg-mesh -j ACCEPT
+      iptables -A FORWARD -o wg-mesh -j ACCEPT
+      iptables -A FORWARD -i wg-net -j ACCEPT
+      iptables -A FORWARD -o wg-net -j ACCEPT
     '';
   };
 
