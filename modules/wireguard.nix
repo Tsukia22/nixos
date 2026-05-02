108b54cc3451b696435e3001f7e9d26a01b83418
Xanedithas <xanedithas@gmail.com>
2026-05-02 14:26:49 +0200

    fix wireguard

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -1,19 +1,30 @@
 { config, pkgs, ... }: {
 
-  boot.kernelModules = [ "wireguard" ];
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "10.100.0.1/24" ];
+    listenPort = 51820;
+    privateKeyFile = "/root/wireguard/private.key";
+    postUp = [
+      "wg addconf wg0 /root/wireguard/wg0-peers.conf"
+      "iptables -A FORWARD -i wg0 -j ACCEPT"
+      "iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
+    ];
+    preDown = [
+      "iptables -D FORWARD -i wg0 -j ACCEPT"
+      "iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
+    ];
+  };
 
-networking.wg-quick.interfaces.wg0.configFile = "/root/wireguard/wg0.conf";
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [ 51820 ];
+    trustedInterfaces = [ "wg0" ];
+    extraCommands = ''
+      iptables -A FORWARD -i wg0 -j ACCEPT
+      iptables -A FORWARD -o wg0 -j ACCEPT
+    '';
+  };
 
-networking.firewall = {
-  enable = true;
-  allowedUDPPorts = [ 51820 ];
-  trustedInterfaces = [ "wg0" ];
-  extraCommands = ''
-    iptables -A FORWARD -i wg0 -j ACCEPT
-    iptables -A FORWARD -o wg0 -j ACCEPT
-  '';
-};
-
-boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
 }
