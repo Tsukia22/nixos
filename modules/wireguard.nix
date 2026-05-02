93a654c22c5fe6d05a7082c782ccd066275444ff
Xanedithas <xanedithas@gmail.com>
2026-05-02 14:16:09 +0200

    fix error

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -2,18 +2,18 @@
 
   boot.kernelModules = [ "wireguard" ];
 
-  networking.wireguard.interfaces.wg0.configFile = "/root/wireguard/wg0.conf";
+networking.wg-quick.interfaces.wg0.configFile = "/root/wireguard/wg0.conf";
 
-  networking.firewall = {
-    enable = true;
-    allowedUDPPorts = [ 51820 ];
-    trustedInterfaces = [ "wg0" ];
-    extraCommands = ''
-      iptables -A FORWARD -i wg0 -j ACCEPT
-      iptables -A FORWARD -o wg0 -j ACCEPT
-    '';
-  };
+networking.firewall = {
+  enable = true;
+  allowedUDPPorts = [ 51820 ];
+  trustedInterfaces = [ "wg0" ];
+  extraCommands = ''
+    iptables -A FORWARD -i wg0 -j ACCEPT
+    iptables -A FORWARD -o wg0 -j ACCEPT
+  '';
+};
 
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
 }
