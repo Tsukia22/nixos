e693a04bde887ed9da6faabfa1724ddb72725242
Xanedithas <xanedithas@gmail.com>
2026-05-24 21:16:01 +0200

    revert wg config

--- modules/wg-mesh.nix
+++ modules/wg-mesh.nix
@@ -4,7 +4,19 @@
   # IP is set at the host config
   # Generate key: wg genkey | tee wg-mesh-private.key | wg pubkey > wg-mesh-public.key
   networking.wg-quick.interfaces.wg-mesh = {
-    configFile = "/root/wireguard/wg-mesh.conf";
+    listenPort = 50001;
+    privateKeyFile = "/root/wireguard/wg-mesh-private.key";
+    postUp = [
+      "wg addconf wg-mesh /root/wireguard/wg-mesh-peers.conf"
+      "iptables -A FORWARD -i wg-mesh -j ACCEPT"
+      "iptables -A FORWARD -o wg-mesh -j ACCEPT"
+      "iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
+    ];
+    preDown = [
+      "iptables -D FORWARD -i wg-mesh -j ACCEPT"
+      "iptables -D FORWARD -o wg-mesh -j ACCEPT"
+      "iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
+    ];
   };
 
   networking.firewall = {
--- modules/wg-net.nix
+++ modules/wg-net.nix
@@ -4,7 +4,19 @@
   # IP is set at the host config
   # Generate key: wg genkey | tee wg-net-private.key | wg pubkey > wg-net-public.key
   networking.wg-quick.interfaces.wg-net = {
-    configFile = "/root/wireguard/wg-net.conf";
+    listenPort = 50002;
+    privateKeyFile = "/root/wireguard/wg-net-private.key";
+    postUp = [
+      "wg addconf wg-net /root/wireguard/wg-net-peers.conf"
+      "iptables -A FORWARD -i wg-net -j ACCEPT"
+      "iptables -A FORWARD -o wg-net -j ACCEPT"
+      "iptables -t nat -A POSTROUTING -s 10.200.0.0/24 -j MASQUERADE"
+    ];
+    preDown = [
+      "iptables -D FORWARD -i wg-net -j ACCEPT"
+      "iptables -D FORWARD -o wg-net -j ACCEPT"
+      "iptables -t nat -D POSTROUTING -s 10.200.0.0/24 -j MASQUERADE"
+    ];
   };
 
   networking.firewall = {
