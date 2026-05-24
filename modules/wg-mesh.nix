6be6b4fc939598af7c92588e9c15d4ceb66f3d62
Xanedithas <xanedithas@gmail.com>
2026-05-24 20:43:04 +0200

    try wg config on machine to enable syncconf

--- modules/wg-mesh.nix
+++ modules/wg-mesh.nix
@@ -4,26 +4,12 @@
   # IP is set at the host config
   # Generate key: wg genkey | tee wg-mesh-private.key | wg pubkey > wg-mesh-public.key
   networking.wg-quick.interfaces.wg-mesh = {
-    listenPort = 50001;
-    privateKeyFile = "/root/wireguard/wg-mesh-private.key";
-    postUp = [
-      "wg addconf wg-mesh /root/wireguard/wg-mesh-peers.conf"
-      "iptables -A FORWARD -i wg-mesh -j ACCEPT"
-      "iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
-    ];
-    preDown = [
-      "iptables -D FORWARD -i wg-mesh -j ACCEPT"
-      "iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -j MASQUERADE"
-    ];
+    configFile = "/root/wireguard/wg-mesh.conf";
   };
 
   networking.firewall = {
     # allowedUDPPorts = [ 50001 ];
     trustedInterfaces = [ "wg-mesh" ];
-    extraCommands = ''
-      iptables -A FORWARD -i wg-mesh -j ACCEPT
-      iptables -A FORWARD -o wg-mesh -j ACCEPT
-    '';
   };
 
 }
--- modules/wg-net.nix
+++ modules/wg-net.nix
@@ -4,26 +4,12 @@
   # IP is set at the host config
   # Generate key: wg genkey | tee wg-net-private.key | wg pubkey > wg-net-public.key
   networking.wg-quick.interfaces.wg-net = {
-    listenPort = 50002;
-    privateKeyFile = "/root/wireguard/wg-net-private.key";
-    postUp = [
-      "wg addconf wg-net /root/wireguard/wg-net-peers.conf"
-      "iptables -A FORWARD -i wg-net -j ACCEPT"
-      "iptables -t nat -A POSTROUTING -s 10.200.0.0/24 -j MASQUERADE"
-    ];
-    preDown = [
-      "iptables -D FORWARD -i wg-net -j ACCEPT"
-      "iptables -t nat -D POSTROUTING -s 10.200.0.0/24 -j MASQUERADE"
-    ];
+    configFile = "/root/wireguard/wg-net.conf";
   };
 
   networking.firewall = {
     # allowedUDPPorts = [ 50002 ];
     trustedInterfaces = [ "wg-net" ];
-    extraCommands = ''
-      iptables -A FORWARD -i wg-net -j ACCEPT
-      iptables -A FORWARD -o wg-net -j ACCEPT
-    '';
   };
 
 }
