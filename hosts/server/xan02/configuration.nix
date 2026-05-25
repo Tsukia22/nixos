fbb87fc5918b7bf90182e310844e081189730a71
Xanedithas <xanedithas@gmail.com>
2026-05-25 16:15:50 +0200

    apply changes to xan02

--- hosts/server/xan02/configuration.nix
+++ hosts/server/xan02/configuration.nix
@@ -52,14 +52,37 @@
     };
   };
 
-  # Networking
-  networking.hostName = "xan02";
-  networking.firewall.allowedTCPPortRanges = [
-    { from = 25550; to = 25559; } # local use
-  ];
-  services.openssh = {
+  # Firewall
+  networking.nftables = {
     enable = true;
-    ports = [ 1993 ];
+    ruleset = ''
+      table inet filter {
+        chain input {
+          type filter hook input priority filter; policy drop;
+
+          # Allow established/related connections
+          ct state established,related accept
+          # Allow loopback
+          iifname "lo" accept
+
+          # wg-mesh: trusted backbone between servers
+          iifname "wg-mesh" ip daddr 10.100.0.0/24 accept
+          iifname "wg-mesh" drop
+
+          # SSH
+          tcp dport 1993 accept
+
+          # Port ranges
+          tcp dport 25560-25564 accept
+
+          # mDNS
+          udp dport 5353 accept
+
+          # ping
+          ip protocol icmp accept
+        }
+      }
+    '';
   };
 
   # Wireguard config
