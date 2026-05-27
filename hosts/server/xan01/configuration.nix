7a257309f8b271c3ef0e5f331307706f2e84c446
Xanedithas <xanedithas@gmail.com>
2026-05-27 23:28:15 +0200

    add immich backup and move volumes backup

--- hosts/server/xan01/configuration.nix
+++ hosts/server/xan01/configuration.nix
@@ -49,16 +49,28 @@
         echo $(date +"%Y-%m-%d %H:%M:%S")
         echo "Backing up volumes..."
 
-        btrfs subvolume snapshot -r /home/kami/.local/share/containers/storage/volumes /var/snapshots/volumes-$(date +%Y%m%d)
+        btrfs subvolume snapshot -r /home/kami/.local/share/containers/storage/volumes /var/snapshots/volumes/volumes-$(date +%Y%m%d)
 
-        PREV=$(ls /var/snapshots/ | sort | tail -2 | head -1)
-        CURR=$(ls /var/snapshots/ | sort | tail -1)
+        PREV=$(ls /var/snapshots/volumes/ | sort | tail -2 | head -1)
+        CURR=$(ls /var/snapshots/volumes/ | sort | tail -1)
 
         echo "$PREV -> $CURR"
 
-        btrfs send -p /var/snapshots/$PREV /var/snapshots/$CURR | ssh xan01@10.100.0.2 -p 1993 "sudo btrfs receive ~/backups/"
+        btrfs send -p /var/snapshots/volumes/$PREV /var/snapshots/volumes/$CURR | ssh xan01@10.100.0.2 -p 1993 "sudo btrfs receive ~/backups/"
 
-        echo "Backup complete."
+        echo "Backup volumes complete."
+        echo "Backing up immich..."
+
+        btrfs subvolume snapshot -r /home/kami/stacks/immich/library/library /var/snapshots/immich/immich-$(date +%Y%m%d)
+
+        PREV=$(ls /var/snapshots/immich/ | sort | tail -2 | head -1)
+        CURR=$(ls /var/snapshots/immich/ | sort | tail -1)
+
+        echo "$PREV -> $CURR"
+
+        btrfs send -p /var/snapshots/immich/$PREV /var/snapshots/immich/$CURR | ssh xan01@10.100.0.2 -p 1993 "sudo btrfs receive ~/backups/"
+
+        echo "Backup immich complete."
       '';
     };
     
