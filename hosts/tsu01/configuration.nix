bb89a12170da2b987ebfeb8f5a2eb36f49273fab
Xanedithas <xanedithas@gmail.com>
2026-06-10 15:45:46 +0200

    enable changes for tsu01

--- hosts/tsu01/configuration.nix
+++ hosts/tsu01/configuration.nix
@@ -11,8 +11,8 @@
     ../../modules/wg-mesh.nix
     ../../modules/wg-net.nix
     ../../modules/dnsmasq.nix
+    ../../modules/host-options.nix
     ./firewall.nix
-    ./backup.nix
   ];
 
   # Bootloader
@@ -52,6 +52,23 @@
   # Wireguard config
   networking.wg-quick.interfaces.wg-mesh.address = [ "10.100.0.3/24" ];
   networking.wg-quick.interfaces.wg-net.address = [ "10.200.0.3/24" ];
+
+  # Host options/configs
+  
+  # Usage example: ${config.host.domain}
+  host.domain = "t.t";
+  host.notify-target = "10.100.0.2";
+  host.notify-key = "3sl7poangptq1mtladsioa";
+
+  # Snapshots require an existing valid btrfs subvolume (manual one-time operation)
+  host.snapshots = {
+    volumes = { from = "/home/kami/.local/share/containers/storage/volumes"; to = "/var/snapshots/volumes"; };
+  };
+
+  # Backups require an existing remote parent snapshot (manual one-time operation)
+  host.backups = {
+    volumes = { remote = "10.100.0.2"; from = "/var/snapshots/volumes"; to = "/mnt/hdd/$HOSTNAME/backups/volumes"; };
+  };
 
   system.stateVersion = "25.05";
 }
